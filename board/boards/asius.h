fbf5e30750b6e7079c7a9e11fd4c50438552e1c2
Karel Nagel <nagelkarel@gmail.com>
2026-04-09 14:00:34 +0300

    rename

--- board/boards/asius.h
+++ board/boards/asius.h
@@ -6,7 +6,7 @@
 // ASIUS (STM32H7) + Harness //
 // ////////////////////////// //
 
-static void cuatro_enable_can_transceiver(uint8_t transceiver, bool enabled) {
+static void asius__enable_can_transceiver(uint8_t transceiver, bool enabled) {
   switch (transceiver) {
     case 1U:
       set_gpio_output(GPIOB, 7, !enabled);
@@ -25,29 +25,29 @@ static void cuatro_enable_can_transceiver(uint8_t transceiver, bool enabled) {
   }
 }
 
-static uint32_t cuatro_read_voltage_mV(void) {
+static uint32_t asius__read_voltage_mV(void) {
   return adc_get_mV(&(const adc_signal_t) ADC_CHANNEL_DEFAULT(ADC1, 8)) * 11U;
 }
 
-static uint32_t cuatro_read_current_mA(void) {
+static uint32_t asius__read_current_mA(void) {
   return adc_get_mV(&(const adc_signal_t) ADC_CHANNEL_DEFAULT(ADC1, 3)) * 2U;
 }
 
-static void cuatro_set_fan_enabled(bool enabled) {
+static void asius__set_fan_enabled(bool enabled) {
   set_gpio_output(GPIOD, 3, !enabled);
 }
 
-static void cuatro_set_bootkick(BootState state) {
+static void asius__set_bootkick(BootState state) {
   set_gpio_output(GPIOA, 0, state != BOOT_BOOTKICK);
   // DC_IN rising edge wakes SOM from ship mode
   set_gpio_output(GPIOC, 11, state != BOOT_BOOTKICK);
 }
 
-static void cuatro_set_amp_enabled(bool enabled) {
+static void asius__set_amp_enabled(bool enabled) {
   set_gpio_output(GPIOB, 0, enabled);
 }
 
-static void cuatro_init(void) {
+static void asius__init(void) {
   common_init_gpio();
 
   // open drain
@@ -75,7 +75,7 @@ static void cuatro_init(void) {
   set_gpio_pullup(GPIOC, 2, PULL_DOWN);
 
   // SOM bootkick + reset lines
-  cuatro_set_bootkick(BOOT_BOOTKICK);
+  asius__set_bootkick(BOOT_BOOTKICK);
 
   // SOM debugging UART
   gpio_uart7_init();
@@ -89,7 +89,7 @@ static void cuatro_init(void) {
   clock_source_init(true);
 
   // Sound codec
-  cuatro_set_amp_enabled(false);
+  asius__set_amp_enabled(false);
   set_gpio_alternate(GPIOA, 2, GPIO_AF8_SAI4);    // SAI4_SCK_B
   set_gpio_alternate(GPIOC, 0, GPIO_AF8_SAI4);    // SAI4_FS_B
   set_gpio_alternate(GPIOD, 11, GPIO_AF10_SAI4);  // SAI4_SD_A
@@ -100,7 +100,54 @@ static void cuatro_init(void) {
   sound_init();
 }
 
-static harness_configuration cuatro_harness_config = {
+
+static void asius_set_can_mode(uint8_t mode) {
+  current_board->enable_can_transceiver(2U, false);
+  current_board->enable_can_transceiver(4U, false);
+  switch (mode) {
+    case CAN_MODE_NORMAL:
+    case CAN_MODE_OBD_CAN2:
+      if ((bool)(mode == CAN_MODE_NORMAL) != (bool)(harness.status == HARNESS_STATUS_FLIPPED)) {
+        // B12,B13: disable normal mode
+        set_gpio_pullup(GPIOB, 12, PULL_NONE);
+        set_gpio_mode(GPIOB, 12, MODE_ANALOG);
+
+        set_gpio_pullup(GPIOB, 13, PULL_NONE);
+        set_gpio_mode(GPIOB, 13, MODE_ANALOG);
+
+        // B5,B6: FDCAN2 mode
+        set_gpio_pullup(GPIOB, 5, PULL_NONE);
+        set_gpio_alternate(GPIOB, 5, GPIO_AF9_FDCAN2);
+
+        set_gpio_pullup(GPIOB, 6, PULL_NONE);
+        set_gpio_alternate(GPIOB, 6, GPIO_AF9_FDCAN2);
+        current_board->enable_can_transceiver(2U, true);
+      } else {
+        // B5,B6: disable normal mode
+        set_gpio_pullup(GPIOB, 5, PULL_NONE);
+        set_gpio_mode(GPIOB, 5, MODE_ANALOG);
+
+        set_gpio_pullup(GPIOB, 6, PULL_NONE);
+        set_gpio_mode(GPIOB, 6, MODE_ANALOG);
+        // B12,B13: FDCAN2 mode
+        set_gpio_pullup(GPIOB, 12, PULL_NONE);
+        set_gpio_alternate(GPIOB, 12, GPIO_AF9_FDCAN2);
+
+        set_gpio_pullup(GPIOB, 13, PULL_NONE);
+        set_gpio_alternate(GPIOB, 13, GPIO_AF9_FDCAN2);
+        current_board->enable_can_transceiver(4U, true);
+      }
+      break;
+    default:
+      break;
+  }
+}
+
+static bool asius_read_som_gpio (void) {
+  return (get_gpio_input(GPIOC, 2) != 0);
+}
+
+static harness_configuration asius__harness_config = {
   .GPIO_SBU1 = GPIOC,
   .GPIO_SBU2 = GPIOA,
   .GPIO_relay_SBU1 = GPIOA,
@@ -113,25 +160,25 @@ static harness_configuration cuatro_harness_config = {
   .adc_signal_SBU2 = ADC_CHANNEL_DEFAULT(ADC1, 17)
 };
 
-board board_cuatro = {
-  .harness_config = &cuatro_harness_config,
+board board_asius_ = {
+  .harness_config = &asius__harness_config,
   .has_spi = true,
   .has_fan = true,
   .avdd_mV = 1800U,
   .fan_enable_cooldown_time = 3U,
-  .init = cuatro_init,
+  .init = asius__init,
   .init_bootloader = unused_init_bootloader,
-  .enable_can_transceiver = cuatro_enable_can_transceiver,
+  .enable_can_transceiver = asius__enable_can_transceiver,
   .led_GPIO = {GPIOC, GPIOC, GPIOC},
   .led_pin = {6, 7, 9},
   .led_pwm_channels = {1, 2, 4},
-  .set_can_mode = tres_set_can_mode,
-  .read_voltage_mV = cuatro_read_voltage_mV,
-  .read_current_mA = cuatro_read_current_mA,
-  .set_fan_enabled = cuatro_set_fan_enabled,
+  .set_can_mode = asius_set_can_mode,
+  .read_voltage_mV = asius__read_voltage_mV,
+  .read_current_mA = asius__read_current_mA,
+  .set_fan_enabled = asius__set_fan_enabled,
   .set_ir_power = unused_set_ir_power,
   .set_siren = fake_siren_set,
-  .set_bootkick = cuatro_set_bootkick,
-  .read_som_gpio = tres_read_som_gpio,
-  .set_amp_enabled = cuatro_set_amp_enabled
+  .set_bootkick = asius__set_bootkick,
+  .read_som_gpio = asius_read_som_gpio,
+  .set_amp_enabled = asius__set_amp_enabled
 };
