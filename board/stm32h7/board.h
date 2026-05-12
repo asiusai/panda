eec959e282dc1d448ebce769c27bc6c7f39d3869
Karel Nagel <nagelkarel@gmail.com>
2026-05-12 15:34:31 +0300

    better board detection

--- board/stm32h7/board.h
+++ board/stm32h7/board.h
@@ -19,13 +19,36 @@
 
 
 void detect_board_type(void) {
-  hw_type = HW_TYPE_ASIUS;
-  current_board = &board_asius;
+  // On STM32H7 pandas, we use two different sets of pins.
+  const uint8_t id1 = detect_with_pull(GPIOF, 7, PULL_UP) |
+                     (detect_with_pull(GPIOF, 8, PULL_UP) << 1U) |
+                     (detect_with_pull(GPIOF, 9, PULL_UP) << 2U) |
+                     (detect_with_pull(GPIOF, 10, PULL_UP) << 3U);
 
-  // WS2812B on PB1: configure early so led_init works.
-  set_gpio_pullup(GPIOB, 1, PULL_NONE);
-  set_gpio_mode(GPIOB, 1, MODE_OUTPUT);
-  set_gpio_output_type(GPIOB, 1, OUTPUT_TYPE_PUSH_PULL);
-  register_set_bits(&(GPIOB->OSPEEDR), GPIO_OSPEEDR_OSPEED1);
-  set_gpio_output(GPIOB, 1, 0);
+  const uint8_t id2 = detect_with_pull(GPIOD, 4, PULL_UP) |
+                     (detect_with_pull(GPIOD, 5, PULL_UP) << 1U) |
+                     (detect_with_pull(GPIOD, 6, PULL_UP) << 2U) |
+                     (detect_with_pull(GPIOD, 7, PULL_UP) << 3U);
+
+  if (id2 == 3U) {
+    hw_type = HW_TYPE_CUATRO;
+    current_board = &board_cuatro;
+  } else if ((id2 & 0x3U) == 0U) {
+    // Asius v1 ties PD4 and PD5 low as board ID pins.
+    hw_type = HW_TYPE_ASIUS;
+    current_board = &board_asius;
+  } else if (id1 == 0U) {
+    hw_type = HW_TYPE_RED_PANDA;
+    current_board = &board_red;
+  } else if (id1 == 1U) {
+    // deprecated
+    //hw_type = HW_TYPE_RED_PANDA_V2;
+    hw_type = HW_TYPE_UNKNOWN;
+  } else if (id1 == 2U) {
+    hw_type = HW_TYPE_TRES;
+    current_board = &board_tres;
+  } else {
+    hw_type = HW_TYPE_UNKNOWN;
+    print("Hardware type is UNKNOWN!\n");
+  }
 }
