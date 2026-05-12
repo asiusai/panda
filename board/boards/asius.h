e23457c479bfe882448011972f7475c191283a11
Karel Nagel <nagelkarel@gmail.com>
2026-05-12 17:57:01 +0300

    fix bootkick

--- board/boards/asius.h
+++ board/boards/asius.h
@@ -31,6 +31,7 @@ static uint32_t asius__read_voltage_mV(void) {
 
 static void asius__set_bootkick(BootState state) {
   UNUSED(state);
+  set_gpio_output(GPIOA, 0, true);
 }
 
 static void asius_gpio_spi_init(void) {
